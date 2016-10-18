d28490a4a734baad3e84f838ac6a6304453211f4
Park Dong-Ha <luncliff@gmail.com>
2016-10-18 10:59:18 +0000

    Mocking `Pages` package
    Done
     - Several page definitions
    
    To Do
     - [TEST]     : JSON Conversion
     - [TEST]     : Page deployment
     - Design simple TCP Networking

---
+++ ManaBob/ManaBob/AppCore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+using ManaBob.ViewModel;
+
+namespace ManaBob
+{
+    public class AppCore : Application
+    {
+        // Net/Local services
+        IService svc;
+
+        public AppCore()
+        {
+            // The root page of your application
+            MainPage = new Pages.IntroPage();
+        }
+
+        // - Note
+        //      Exit the Core. Release all resources
+        public void exit()
+        {
+            // ...
+        }
+
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+
+}// namespace ManaBob
