1433e564631d01e2e3e91bb17c83ee01336a3c83
Park Dong-Ha <luncliff@gmail.com>
2016-10-30 23:26:35 +0900

    Mocking Pages with Xamarin.Forms

--- ManaBob/ManaBob/AppCore.cs
+++ ManaBob/ManaBob/AppCore.cs
@@ -14,10 +14,14 @@ namespace ManaBob
         // Net/Local services
         IService svc;
 
+        ContentPage _intro  = new Pages.IntroPage();
+        // ContentPage _login  = new Pages.LoginPage();
+        // ContentPage _web    = new Pages.WebPage();
+
         public AppCore()
         {
             // The root page of your application
-            MainPage = new Pages.IntroPage();
+            MainPage = new NavigationPage(_intro);
         }
 
         // - Note
