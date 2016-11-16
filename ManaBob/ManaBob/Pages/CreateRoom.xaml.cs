937ca75211b2e5b50f6ee424d747ad0904c89823
USER <USER@DESKTOP-PNT28EO>
2016-11-16 20:18:00 +0900

    Label 수정하였습니다  -장병욱, 금영환

--- ManaBob/ManaBob/Pages/CreateRoom.xaml.cs
+++ ManaBob/ManaBob/Pages/CreateRoom.xaml.cs
@@ -8,18 +8,18 @@ using Xamarin.Forms;
 
 namespace ManaBob.Pages
 {
-	public partial class CreateRoom : ContentPage
-	{
+    public partial class CreateRoom : ContentPage
+    {
         Navigator navi;
         Repository pages;
-		public CreateRoom (Navigator _navi, Repository _pages)
-		{
+        public CreateRoom(Navigator _navi, Repository _pages)
+        {
             navi = _navi;
             pages = _pages;
 
-			InitializeComponent ();
-            
-             var strings = new List<String>
+            InitializeComponent();
+
+            var strings = new List<String>
              {
                  "한식",
                  "중식",
@@ -28,10 +28,23 @@ namespace ManaBob.Pages
                  "학식"
              };
 
-             foreach (var str in strings)
-             {
+            foreach (var str in strings)
+            {
                 category.Items.Add(str);
-             }
+            }
+
+            var per = new List<String>
+             {
+                 "2명",
+                 "3명",
+                 "4명",
+                 "기타"
+             };
+
+            foreach (var str in per)
+            {
+                person.Items.Add(str);
+            }
         }
     }
 }
